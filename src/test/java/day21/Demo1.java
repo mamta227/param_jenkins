af2e964a21844f12d4b2b34fc6c38ae3da270564
mamat <mamat@NU14A1.mshome.net>
2023-06-25 19:22:06 +0530

    extent report

--- src/test/java/day21/Demo1.java
+++ src/test/java/day21/Demo1.java
@@ -12,22 +12,34 @@ import org.testng.Reporter;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
 public class Demo1 {
 	
 	@Parameters({"gridURL","appURL"})
 	@Test
 	public void testA(String gridURL, String appURL) throws InterruptedException, MalformedURLException
-	{
-		Reporter.log("testA of Demo1 of day21", true);
+	{ 
+		ExtentReports report=new ExtentReports();
+		ExtentSparkReporter spark=new ExtentSparkReporter("report/Spark.html");
+		report.attachReporter(spark);
+		ExtentTest extentTest=report.createTest("testA");
+		extentTest.info("testA of Demo1 of day21");
 		WebDriver driver=new RemoteWebDriver(new URL(gridURL), new ChromeOptions());
 		driver.get(appURL);
-		System.out.println(driver.getTitle());
+		
+		extentTest.info(driver.getTitle());
+		
+		extentTest.info("Enter username , pw and click login");
 		driver.findElement(By.id("username")).sendKeys("admin");
 		driver.findElement(By.name("pwd")).sendKeys("manager");
 		driver.findElement(By.id("loginButton")).click();
 		Thread.sleep(7000);
-		System.out.println(driver.getTitle());
+		extentTest.info(driver.getTitle());
 		driver.quit();
+		report.flush();
 		
 	}
 
